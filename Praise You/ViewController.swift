775d7b7d286783457a5ad9a925787c8306f992b5
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-12-10 16:45:28 +0800

    Initial Commit

---
+++ Praise You/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Praise You
+//
+//  Created by Svitlana Dzyuban on 10/12/18.
+//  Copyright © 2018 Lana Dzyuban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
